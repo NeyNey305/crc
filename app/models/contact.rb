79eebe7a305e34280cea9b3b450df5c9aa95b900
Jenee Washington <jeneewashington@icloud.com>
2017-02-24 16:17:14 -0500

    Updating CSS and formatting of contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,14 @@
 class Contact < ApplicationRecord
+
+  # include MailForm::Delivery
+  #
+  # append :remote_ip, :user_agent, :session
+  # attributes :first_name, :last_name, :email, :phone_number, :message, :created_at
+  #
+  # def headers
+  #   {
+  #     :to => "your.email@your.domain.com",
+  #     :subject => "User sent a message"
+  #   }
+  # end
 end
